168bd70fbe6b2e87a9f36ed36d89e652d5a40d7e
Sujal <82680541+sjlhexa@users.noreply.github.com>
2023-05-26 00:18:12 +0530

    Add files via upload

---
+++ 17_quiz _game_oop/question_model.py
@@ -0,0 +1,9 @@
+class Question():
+    def __init__(self, text, answer):
+        self.que = text
+        self.ans = answer
+
+
+
+#new_q = Question("is harry potter the best", "True")
+#print(new_q.que)
